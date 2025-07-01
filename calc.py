fa89f6bb3b5bf16e0e76a5d7200cc1d23dafcc4d
shitomi <shitomi.hyonosuke.tkb@gmail.com>
2025-07-01 22:36:58 +0900

    create prime list

--- calc.py
+++ calc.py
@@ -4,6 +4,22 @@ def calc_gcd(a, b):
     
     return a
 
+def list_prime(a):
+    if a < 2:
+        return []
+    
+    is_prime = [True] * (a + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(a**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, a+1, i):
+                is_prime[j] = False
+
+    primes = [i for i, val in enumerate(is_prime) if val]
+
+    return primes
+
 print("input the number")
 a=int(input())
 print("a is " + str(a))
@@ -19,5 +35,7 @@ print("Division:a/b="+ str(a/b))
 gcd = calc_gcd(a,b)
 print("GCD(a,b) is "+ str(gcd))
 
-
-
+prime_a = list_prime(a)
+prime_b = list_prime(b)
+print("prime number (<=a) is " + str(prime_a))
+print("prime number (<=b) is " + str(prime_b))
