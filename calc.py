48479109beb9386cc543a0a2e066fbfff58d7a36
shitomi <shitomi.hyonosuke.tkb@gmail.com>
2025-07-01 22:30:25 +0900

    create gcd

--- calc.py
+++ calc.py
@@ -1,3 +1,9 @@
+def calc_gcd(a, b):
+    while b:
+        a, b = b, a%b
+    
+    return a
+
 print("input the number")
 a=int(input())
 print("a is " + str(a))
@@ -9,3 +15,9 @@ print("Addition:a+b="+ str(a+b))
 print("Substruction:a-b="+ str(a-b))
 print("Multiplication:a*b="+str(a*b))
 print("Division:a/b="+ str(a/b))
+
+gcd = calc_gcd(a,b)
+print("GCD(a,b) is "+ str(gcd))
+
+
+
